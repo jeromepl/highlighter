3153dc15743c3cb4c0f343153fb31a077f18bf9f
jeromepl <jerome-pl2@hotmail.ca>
2020-03-13 23:35:33 -0400

    Fix issues with highlighting after removing all highlights from page

--- contentScripts/removeHighlights.js
+++ contentScripts/removeHighlights.js
@@ -1,5 +1,7 @@
 "use strict";
 
 // Remove Highlights
-$('.highlighter--highlighted').contents().unwrap();
 clearPage(window.location.hostname + window.location.pathname, window.location.pathname);
+// Force a reload here in order to get the DOM elements back in their original form
+// This is important since this whole thing depends on having reliable query strings
+window.location.reload(false);
