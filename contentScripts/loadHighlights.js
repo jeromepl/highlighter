a4a96cf29519ddc3e354dc1ad76a0b3133f24d2e
jeromepl <jerome-pl2@hotmail.ca>
2020-03-13 22:34:55 -0400

    Fix wrong url used as storage key

--- contentScripts/loadHighlights.js
+++ contentScripts/loadHighlights.js
@@ -1,5 +1,5 @@
 "use strict";
 
 $(document).ready(function() {
-    loadAll(window.location.pathname);
+    loadAll(window.location.hostname + window.location.pathname, window.location.pathname);
 });
