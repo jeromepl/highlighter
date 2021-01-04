d83c7e067df1bcd8608ce0e4dadf423806884ea3
jeromepl <jerome-pl2@hotmail.ca>
2021-01-04 14:55:33 -0500

    Add more keyboard shortcuts
    Closes #10

--- background.js
+++ background.js
@@ -17,9 +17,31 @@ chrome.storage.sync.get('color', (values) => {
 
 // Add Keyboard shortcut
 chrome.commands.onCommand.addListener(function(command) {
-    if (command === "execute-highlight") {
-        trackEvent('highlight-source', 'keyboard-shortcut');
-        highlightText();
+    switch(command) {
+        case 'execute-highlight':
+            trackEvent('highlight-source', 'keyboard-shortcut');
+            highlightText();
+            break;
+        case 'toggle-highlighter-cursor':
+            trackEvent('toggle-cursor-source', 'keyboard-shortcut');
+            toggleHighlighterCursor();
+            break;
+        case 'change-color-to-yellow':
+            trackEvent('color-change-source', 'keyboard-shortcut');
+            changeColor('yellow');
+            break;
+        case 'change-color-to-cyan':
+            trackEvent('color-change-source', 'keyboard-shortcut');
+            changeColor('cyan');
+            break;
+        case 'change-color-to-lime':
+            trackEvent('color-change-source', 'keyboard-shortcut');
+            changeColor('lime');
+            break;
+        case 'change-color-to-magenta':
+            trackEvent('color-change-source', 'keyboard-shortcut');
+            changeColor('magenta');
+            break;
     }
 });
 
