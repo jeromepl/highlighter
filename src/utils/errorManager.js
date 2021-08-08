4daa054a96b27483bad42c3bb368923239cce59a
Jerome Parent-Levesque <jerome-pl2@hotmail.ca>
2021-08-08 13:30:06 -0400

    Refactor error manager

--- src/utils/errorManager.js
+++ src/utils/errorManager.js
@@ -3,25 +3,18 @@
 const MAX_RETRY_TIME = 10000; // Stop trying to highlight after this time (in ms)
 const RETRY_INTERVAL = 500;
 
-const highlightErrors = [];
-
 function addHighlightError(highlight, highlightIndex) { /* eslint-disable-line no-redeclare, no-unused-vars */
-    highlightErrors.push({
+    const highlightError = {
         highlight,
         highlightIndex,
         errorTime: Date.now(),
-    });
+    };
+    setTimeout(retryHighlightError, RETRY_INTERVAL, highlightError);
 }
 
-setInterval(() => {
-    highlightErrors.forEach((highlightError, idx) => {
-        if (Date.now() - highlightError.errorTime > MAX_RETRY_TIME) { // Stop the search
-            highlightErrors.splice(idx, 1);
-        } else { // Keep retrying
-            const success = load(highlightError.highlight, highlightError.highlightIndex, true);
-            if (success) {
-                highlightErrors.splice(idx, 1);
-            }
-        }
-    });
-}, RETRY_INTERVAL);
+function retryHighlightError(highlightError) {
+    const success = load(highlightError.highlight, highlightError.highlightIndex, true);
+    if (!success && Date.now() - highlightError.errorTime < MAX_RETRY_TIME) {
+        setTimeout(retryHighlightError, RETRY_INTERVAL, highlightError);
+    }
+}
