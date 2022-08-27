a643f6e7dd9bccea5636f6aea78094688692a5b6
jeromepl <jerome-pl2@hotmail.ca>
2022-08-27 19:42:34 -0400

    Fix typo

--- src/background/actions/getHighlights.js
+++ src/background/actions/getHighlights.js
@@ -2,7 +2,7 @@ import { executeInCurrentTab } from '../utils.js';
 
 function getHighlights() {
     function contentScriptGetHighlights() {
-        return window.highligherAPI.getPageHighlights();
+        return window.highlighterAPI.getPageHighlights();
     }
 
     return executeInCurrentTab({ func: contentScriptGetHighlights });
