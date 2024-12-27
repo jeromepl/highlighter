c830734eee83947df9e0fb1a34e3a191c4028f89
jeromepl <jerome-pl2@hotmail.ca>
2024-12-27 16:09:04 -0500

    Cleanup the hacky test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,11 +1,6 @@
 import { test, expect } from './fixtures';
 import { selectText } from './utils';
 
-// test('example test', async ({ page }) => {
-//   await page.goto('https://example.com');
-//   await expect(page.locator('body')).toHaveText('Changed by my-extension');
-// });
-
 test.describe('popup', () => {
   test('popup page', async ({ page, extensionId }) => {
     await page.goto(`chrome-extension://${extensionId}/src/popup/index.html`);
@@ -14,26 +9,15 @@ test.describe('popup', () => {
 });
 
 test.describe('highlight', () => {
-  test('test', async ({ context, backgroundWorker, extensionId, page }) => {
+  test('highlights selected text', async ({ popupPage, page }) => {
     await page.goto('https://example.com');
 
     await selectText(page, 'You may use this');
 
-    backgroundWorker.on('console', msg => console.log(msg.text()));
+    await popupPage.evaluate(() => {
+      chrome.runtime.sendMessage({ action: 'highlight' });
+    });
 
-    await backgroundWorker.evaluate(async (extensionId) => {
-      const activeTab = (await chrome.tabs.query({ active: true, currentWindow: true }))[0];
-      await chrome.tabs.sendMessage(activeTab.id, {
-        method: 'highlight.create',
-        params: [{
-          title: 'yellow',
-          color: 'rgb(255, 246, 21)',
-        }],
-      });
-    }, extensionId);
-
-    // await page.keyboard.press("Alt+H");
-
-    await page.pause();
+    await expect(page.locator('highlighter-span')).toHaveText("You may use this");
   });
 });
--- tests/fixtures.js
+++ tests/fixtures.js
@@ -29,5 +29,16 @@ export const test = base.extend({
     const extensionId = backgroundWorker.url().split('/')[2];
     await use(extensionId);
   },
+  // There are a lot of limitations to testing chrome extensions with playwright.
+  // One of those is that we can't open the popup page in the same way a user would.
+  // Instead, we have to open the popup in a new tab, and make sure that our code is able to handle that,
+  // by launching the content scripts in the correct (active) tab.
+  // Note that we also can't (for some reason) send messages to the service worker through the main page,
+  // so we have to do it through the popup page. Otherwise chrome.runtime returns null.
+  popupPage: async({ context, extensionId }, use) => {
+    const page = await context.newPage();
+    await page.goto(`chrome-extension://${extensionId}/src/popup/index.html`);
+    await use(page);
+  }
 });
 export const expect = test.expect;
